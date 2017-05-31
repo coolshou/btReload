0cf01c4f83b2db1da865d2cef8ecb461bc291135
jimmy <jimmy_yeh@alphanetworks.com>
2017-05-31 11:15:51 +0800

    add test_bitcomit.py

---
+++ test_bitcomit.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 10:07:06 2017
+
+@author: 10007014
+"""
+
+from bitcomit import bitcomit
+# main
+if __name__ == '__main__':
+    
+    #p = "D:\中文.txt"
+    p = "D:\\中文\\"
+    bt = bitcomit("http://127.0.0.1", "12345", "admin", "123456", 60)
+    bt.remove(p)
